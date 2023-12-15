b5b2d1a835a37599b089bd034efd9b2c0b3319c7
shaynalene <shaynalenep@gmail.com>
2023-12-15 23:21:57 +0800

    Change Grid Number Button Prompt
    Added a button that will change the number of rows and columns on the gird. Created a load grid function.

--- script.js
+++ script.js
@@ -2,26 +2,46 @@ const cntr = document.querySelector("#grid-container");
 
 var gridno = 16;
 
-for (var i = 0; i < 16; i++) {
-  const squareDivCtr = document.createElement("div");
-  squareDivCtr.id = "sqdivctr";
-  squareDivCtr.setAttribute("style", "display: flex; flex-direction: column;");
-  cntr.appendChild(squareDivCtr);
-  for (var j = 0; j < 16; j++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.id = "sqdiv";
-    squareDiv.setAttribute("style", "border: 2px solid; padding: 1em;");
+loadgrid();
 
-    squareDivCtr.appendChild(squareDiv);
+function loadgrid() {
+  for (var i = 0; i < gridno; i++) {
+    const squareDivCtr = document.createElement("div");
+    squareDivCtr.id = "sqdivctr";
+    squareDivCtr.setAttribute(
+      "style",
+      "display: flex; flex-direction: column;"
+    );
+    cntr.appendChild(squareDivCtr);
+    for (var j = 0; j < gridno; j++) {
+      const squareDiv = document.createElement("div");
+      squareDiv.id = "sqdiv";
+      squareDiv.setAttribute("style", "border: 2px solid; padding: 1em;");
+
+      squareDivCtr.appendChild(squareDiv);
+    }
+  }
+
+  const squares = document.querySelectorAll("#sqdiv");
+
+  squares.forEach((element) => {
+    element.addEventListener("mouseover", function (e) {
+      e.target.style.background = "blue";
+    });
+  });
+  {
   }
 }
 
-const squares = document.querySelectorAll("#sqdiv");
+const btn = document.querySelector("#btn-grid");
 
-squares.forEach((element) => {
-  element.addEventListener("mouseover", function (e) {
-    e.target.style.background = "blue";
-  });
+btn.addEventListener("click", function (e) {
+  let ipt = prompt("Enter number of rows and columns: ");
+  let tempgridno = gridno;
+  for (var i = 0; i < tempgridno; i++) {
+    let squares = document.querySelector("#sqdivctr");
+    cntr.removeChild(squares);
+  }
+  gridno = ipt;
+  loadgrid();
 });
-{
-}
